5d86919875f88531b10d3b4687c1861abb98476d
tsong0ku <sonbappe89@gmail.com>
2023-11-22 21:41:55 +0700

    search feature

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,131 +1,197 @@
-import { useState } from "react"
-import { useSelector, useDispatch } from 'react-redux'
-import todoSlice, { addTask,deleteTask,updateTask } from "./features/todoSlice"
-import './App.css'
-import './Modal.css'
-import './responsive.css'
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import todoSlice, {
+  addTask,
+  deleteTask,
+  updateTask,
+  searchTask,
+} from "./features/todoSlice";
+import "./App.css";
+import "./Modal.css";
+import "./responsive.css";
 export default function TodoApp() {
+  const [input, setInput] = useState("");
+  const dispatch = useDispatch();
+  const [validationError, setValidationError] = useState("");
+  const [selectedTask, setSelectedTask] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [priority, setPriority] = useState("Medium");
+  const [searchQuery, setSearchQuery] = useState("");
 
-const [input, setInput] = useState('')
-const dispatch = useDispatch()
-const [validationError, setValidationError] = useState(''); 
-const [selectedTask,setSelectedTask] = useState(null)
-const [showModal,setShowModal] = useState(false)
-const [priority,setPriority] = useState('Medium')
+  function handleSubmit(e) {
+    if (input.trim() !== "") {
+      e.preventDefault();
+      dispatch(addTask({ text: input, id: Date.now(), priority: priority }));
 
-
-function handleSubmit(e){
-  if (input.trim() !== '') {
-
-  e.preventDefault()
-  dispatch(addTask({text: input, id: Date.now(), priority:priority}))
-
-  setInput('')
-  setPriority('Medium')
-setValidationError('');
-
-  }else{
-    setValidationError('Fill something please ...');
-    e.preventDefault()
-
-  }
-}
-const list = useSelector((state) => state.todo.list)
-
-function handleDeleteTask(id){
-  dispatch(deleteTask(id))
-}
-
-function handleEditTask(task){
-  setSelectedTask(task)
-  setShowModal(true)
-}
-
-function handleSaveEditedTask(){
-  dispatch(updateTask({id:selectedTask.id , text:selectedTask.text ,priority:selectedTask.priority}))
-
-  setSelectedTask(null)
-  setShowModal(false)
-}
-
-function handleCloseModal(){
-  setShowModal(false)
-}
-return(
-  <>
-  <div className="app">
-    <div className="app-form">
-  <form id="form-add" action="" onSubmit={handleSubmit} >
-  <div className="form-add-row">
-  <input className="app-form-input" placeholder="Input a task ..." type="text" value={input} onChange={(e)=>{setInput(e.target.value)}} />
-  
-  <label htmlFor="">
-    <span className="priority">Priority:</span> 
-    <select value={priority} onChange={(e)=>{setPriority(e.target.value)}}>
-      <option value="High">High</option>
-      <option value="Medium">Medium</option>
-      <option value="Low">Low</option>
-
-    </select>
-    </label>
-
-  <button className="btn-add" type="submit">ADD</button>
-
-  </div>
-  {validationError &&  <div className="errorMsg" style={{ color: 'red' }}>{validationError}
-  </div>}
-  </form>
-
-  </div>
- <div  className="app-result">
-    
-    <ol  className="app-list">
-      {list.map((task) => (
-        <div  key={task.id} className="app-list-item">
-        <li  >{task.text}
-        
-        
-        </li>
-        <div className="app-list-priority">
-          {task.priority}
-        </div>
-        <div className="btn-group">
-
-        <button className="btn-edit" onClick={()=>{handleEditTask(task)}}>EDIT</button>
-        <button className="btn-delete" onClick={()=>{handleDeleteTask(task.id)}}>DELETE</button>
-        </div>
-        </div>
-      ))}
-    </ol>
-    {showModal && (
-          <div className="modal" onClick={handleCloseModal}>
-          <div className="modal-dialog" onClick={e => e.stopPropagation()}>
-            <div className="modal-header">
-              <h3 className="modal-title">EDIT TASK</h3>
-              <span className="modal-close" onClick={handleCloseModal}>
-                &times;
-              </span>
-            </div>
-            <div className="modal-body">
-              <div className="modal-content">
-              <input className="modal-input" type="text" value={selectedTask.text} placeholder="Input new task name"
-             onChange={(e)=> setSelectedTask({...selectedTask,text: e.target.value})}/>
-              <input className="modal-input" type="text" value={selectedTask.priority} placeholder="Input new task priority"
-             onChange={(e)=> setSelectedTask({...selectedTask,priority: e.target.value})}/>
-  
-              </div>
-            </div>
-           <div className="modal-footer">
-             <button className="btn-save" onClick={handleSaveEditedTask}>Save</button>
-              
-              </div>
-   
- </div>
-  </div>
-)
+      setInput("");
+      setPriority("Medium");
+      setValidationError("");
+    } else {
+      setValidationError("Fill something please ...");
+      e.preventDefault();
     }
-    </div>
-    </div>
-</>
-)
+  }
+  const list = useSelector((state) => state.todo.list);
+
+  function handleDeleteTask(id) {
+    dispatch(deleteTask(id));
+  }
+
+  function handleEditTask(task) {
+    setSelectedTask(task);
+    setShowModal(true);
+  }
+
+  function handleSaveEditedTask() {
+    dispatch(
+      updateTask({
+        id: selectedTask.id,
+        text: selectedTask.text,
+        priority: selectedTask.priority,
+      })
+    );
+
+    setSelectedTask(null);
+    setShowModal(false);
+  }
+
+  function handleCloseModal() {
+    setShowModal(false);
+  }
+
+  function handleSearch(e) {
+    setSearchQuery(e.target.value);
+  }
+
+  const filteredTodos = list.filter((task) =>
+    task.text.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+  return (
+    <>
+      <div className="app">
+        <div className="app-form">
+          <form id="form-add" action="" onSubmit={handleSubmit}>
+            <div className="form-add-row">
+              <input
+                className="app-form-input"
+                placeholder="Input a task ..."
+                type="text"
+                value={input}
+                onChange={(e) => {
+                  setInput(e.target.value);
+                }}
+              />
+              <label htmlFor="">
+                <span className="priority">Priority:</span>
+                <select
+                  value={priority}
+                  onChange={(e) => {
+                    setPriority(e.target.value);
+                  }}
+                >
+                  <option value="High">High</option>
+                  <option value="Medium">Medium</option>
+                  <option value="Low">Low</option>
+                </select>
+              </label>
+
+              <button className="btn-add" type="submit">
+                ADD
+              </button>
+              <label htmlFor="">
+                Search by task name:
+                <input
+                  type="text"
+                  value={searchQuery}
+                  onChange={handleSearch}
+                  placeholder="Search tasks"
+                />
+              </label>
+            </div>
+            {validationError && (
+              <div className="errorMsg" style={{ color: "red" }}>
+                {validationError}
+              </div>
+            )}
+          </form>
+        </div>
+        <div className="app-result">
+          <ol className="app-list">
+            {filteredTodos.map((task) => (
+              <div key={task.id} className="app-list-item">
+                <li>{task.text}</li>
+                <div className="app-list-priority">{task.priority}</div>
+                <div className="btn-group">
+                  <button
+                    className="btn-edit"
+                    onClick={() => {
+                      handleEditTask(task);
+                    }}
+                  >
+                    EDIT
+                  </button>
+                  <button
+                    className="btn-delete"
+                    onClick={() => {
+                      handleDeleteTask(task.id);
+                    }}
+                  >
+                    DELETE
+                  </button>
+                </div>
+              </div>
+            ))}
+          </ol>
+          {showModal && (
+            <div className="modal" onClick={handleCloseModal}>
+              <div
+                className="modal-dialog"
+                onClick={(e) => e.stopPropagation()}
+              >
+                <div className="modal-header">
+                  <h3 className="modal-title">EDIT TASK</h3>
+                  <span className="modal-close" onClick={handleCloseModal}>
+                    &times;
+                  </span>
+                </div>
+                <div className="modal-body">
+                  <div className="modal-content">
+                    <input
+                      className="modal-input"
+                      type="text"
+                      value={selectedTask.text}
+                      placeholder="Input new task name"
+                      onChange={(e) =>
+                        setSelectedTask({
+                          ...selectedTask,
+                          text: e.target.value,
+                        })
+                      }
+                    />
+                    <input
+                      className="modal-input"
+                      type="text"
+                      value={selectedTask.priority}
+                      placeholder="Input new task priority"
+                      onChange={(e) =>
+                        setSelectedTask({
+                          ...selectedTask,
+                          priority: e.target.value,
+                        })
+                      }
+                    />
+                  </div>
+                </div>
+                <div className="modal-footer">
+                  <button className="btn-save" onClick={handleSaveEditedTask}>
+                    Save
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
 }
--- src/features/todoSlice.js
+++ src/features/todoSlice.js
@@ -1,38 +1,37 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  list:[]
-}
+  list: [],
+  searchQuery: "",
+};
 
 export const counterSlice = createSlice({
-  name: 'todo',
+  name: "todo",
   initialState,
   reducers: {
-    addTask: (state,action) => {
+    addTask: (state, action) => {
       // Redux Toolkit allows us to write "mutating" logic in reducers. It
       // doesn't actually mutate the state because it uses the Immer library,
       // which detects changes to a "draft state" and produces a brand new
       // immutable state based off those changes
-      state.list.push(action.payload)
+      state.list.push(action.payload);
     },
-    deleteTask:(state,action)=>{
-      state.list=state.list.filter((todo) => todo.id !== action.payload)
+    deleteTask: (state, action) => {
+      state.list = state.list.filter((todo) => todo.id !== action.payload);
     },
-   updateTask:(state,action)=>{
-   const {id,text,priority} = action.payload
-   const selectedUpdateTask = state.list.find((task) => task.id === id)
-   if(selectedUpdateTask){
-
-   selectedUpdateTask.text = text
-   selectedUpdateTask.priority = priority
-   }
-  }
-
+    updateTask: (state, action) => {
+      const { id, text, priority } = action.payload;
+      const selectedUpdateTask = state.list.find((task) => task.id === id);
+      if (selectedUpdateTask) {
+        selectedUpdateTask.text = text;
+        selectedUpdateTask.priority = priority;
+      }
+    },
   },
-})
+});
 
 // Action creators are generated for each case reducer function
-export const { addTask, deleteTask,updateTask } = counterSlice.actions
+export const { addTask, deleteTask, updateTask, searchTask } =
+  counterSlice.actions;
 
-
-export default counterSlice.reducer
+export default counterSlice.reducer;
